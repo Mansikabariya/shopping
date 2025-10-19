866539035c38780c90a23fadf06f05ddcb1466fe
Mansi <mansikabariya5588@gmail.com>
2025-10-19 21:37:37 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/shopping/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shopping.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:shopping/app/app.dart';
+import 'package:shopping/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
